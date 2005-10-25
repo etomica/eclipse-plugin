6bcd64c6b2466006aa51592f8d5c2312a5374d5f
Andrew Schultz <ajs42@buffalo.edu>
2005-10-25 01:42:30 +0000

    make Simulation"View" a object-only but complete hierarchy of the simulation

--- etomica.plugin/src/etomica/plugin/views/SimulationViewContentProvider.java
+++ etomica.plugin/src/etomica/plugin/views/SimulationViewContentProvider.java
@@ -4,13 +4,23 @@
  */
 package etomica.plugin.views;
 
+import java.util.LinkedList;
+
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
 
+import etomica.action.activity.ActivityGroup;
+import etomica.compatibility.FeatureSet;
+import etomica.plugin.wrappers.ArrayWrapper;
 import etomica.plugin.wrappers.PropertySourceWrapper;
 import etomica.simulation.SimulationEvent;
 import etomica.simulation.SimulationListener;
+import etomica.space.Vector;
+import etomica.util.Arrays;
+import etomica.util.EnumeratedType;
 
 /**
  * TODO To change the template for this generated type comment go to
@@ -28,7 +38,50 @@ public class SimulationViewContentProvider implements ITreeContentProvider, Simu
 	 * ActivityGroups are parents of actions/activities
 	 */
 	public Object[] getChildren(Object wrappedElement) {
-        return ((PropertySourceWrapper)wrappedElement).getChildren();
+        if (wrappedElement instanceof ArrayWrapper) {
+            return ((ArrayWrapper)wrappedElement).getChildren();
+        }
+        if (((PropertySourceWrapper)wrappedElement).getObject() instanceof ActivityGroup) {
+            // ActivityGroup has (get){Completed,Pending,Current}Action, which we don't care about
+            // the wrapper will give us AllActions, which is what we want
+            return ((PropertySourceWrapper)wrappedElement).getChildren();
+        }
+        IPropertyDescriptor[] descriptors = ((PropertySourceWrapper)wrappedElement).getPropertyDescriptors();
+        int count = 0;
+        PropertySourceWrapper[] childWrappers = new PropertySourceWrapper[0];
+        for (int i=0; i<descriptors.length; i++) {
+            Object pd = descriptors[i].getId();
+            Object value = ((PropertySourceWrapper)wrappedElement).getPropertyValue(pd);
+            if (value == null) {
+                continue;
+            }
+            if (value.getClass().isArray()) {
+                if (!(value instanceof Object[])) {
+                    continue;
+                }
+                if (((Object[])value).length == 0) {
+                    continue;
+                }
+            }
+            else if (value instanceof Number ||
+                    value instanceof Boolean ||
+                    value instanceof Color ||
+                    value instanceof Vector ||
+                    value instanceof EnumeratedType ||
+                    value instanceof String ||
+                    value instanceof FeatureSet ||
+                    value instanceof LinkedList) {
+                continue;
+            }
+            childWrappers = (PropertySourceWrapper[])Arrays.resizeArray(childWrappers,++count);
+            if (value instanceof PropertySourceWrapper) {
+                childWrappers[count-1] = (PropertySourceWrapper)value;
+            }
+            else {
+                childWrappers[count-1] = PropertySourceWrapper.makeWrapper(value);
+            }
+        }
+        return childWrappers;
 	}
     
     /**
@@ -38,7 +91,7 @@ public class SimulationViewContentProvider implements ITreeContentProvider, Simu
     //the call to viewer.setInput in createPartControl causes the list of
     //simulation instances to be the input element in this method
     public Object[] getElements(Object inputElement) {
-        return ((PropertySourceWrapper)inputElement).getChildren();
+        return getChildren(inputElement);
     }
 
 
@@ -54,7 +107,8 @@ public class SimulationViewContentProvider implements ITreeContentProvider, Simu
 	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
 	 */
 	public boolean hasChildren(Object wrappedElement) {
-        return ((PropertySourceWrapper)wrappedElement).getChildren().length > 0;
+        // this will return false positives for non-Object or null children
+        return ((PropertySourceWrapper)wrappedElement).getPropertyDescriptors().length > 0;
 	}
 	
 
