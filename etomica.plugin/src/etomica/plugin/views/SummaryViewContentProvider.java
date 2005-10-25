3f0119dafc7be23c445297d03ccc60eca713f09d
Andrew Schultz <ajs42@buffalo.edu>
2005-10-25 01:38:38 +0000

    add Summary view and content provider -- top-level view of the simulation

---
+++ etomica.plugin/src/etomica/plugin/views/SummaryViewContentProvider.java
@@ -0,0 +1,85 @@
+/*
+ * History
+ * Created on Sep 20, 2004 by kofke
+ */
+package etomica.plugin.views;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+
+import etomica.plugin.wrappers.PropertySourceWrapper;
+import etomica.simulation.SimulationEvent;
+import etomica.simulation.SimulationListener;
+
+/**
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class SummaryViewContentProvider implements ITreeContentProvider, SimulationListener {
+
+    public SummaryViewContentProvider() {
+        //Simulation.instantiationEventManager.addListener(this); 
+        
+    }
+	/**
+	 * Simulation is root.
+	 * Controller is child of simulation.
+	 * ActivityGroups are parents of actions/activities
+	 */
+	public Object[] getChildren(Object wrappedElement) {
+        return ((PropertySourceWrapper)wrappedElement).getChildren();
+	}
+    
+    /**
+     * @param inputElement a linked list containing the simulation instances,
+     * coming from Simulation.getInstances
+     */
+    //the call to viewer.setInput in createPartControl causes the list of
+    //simulation instances to be the input element in this method
+    public Object[] getElements(Object inputElement) {
+        return ((PropertySourceWrapper)inputElement).getChildren();
+    }
+
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+	 */
+	public Object getParent(Object element) {
+		System.out.println("SimulationViewContentProvide.getParent");
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+	 */
+	public boolean hasChildren(Object wrappedElement) {
+        return ((PropertySourceWrapper)wrappedElement).getChildren().length > 0;
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+	 */
+	public void dispose() {
+        //Simulation.instantiationEventManager.removeListener(this);
+		viewer = null;
+	}
+
+    
+    public void actionPerformed(SimulationEvent evt) {
+        viewer.refresh();
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+     */
+    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+        this.viewer = (TreeViewer)viewer;
+        currentSelection = newInput;
+    }
+    
+    Object currentSelection;
+
+	private TreeViewer viewer;
+}
