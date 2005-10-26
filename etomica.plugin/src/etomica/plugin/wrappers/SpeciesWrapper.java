21f1635c763678b1074d84656f2ab096b7402198
Andrew Schultz <ajs42@buffalo.edu>
2005-10-26 01:27:28 +0000

    screen out SpeciesSignature

--- etomica.plugin/src/etomica/plugin/wrappers/SpeciesWrapper.java
+++ etomica.plugin/src/etomica/plugin/wrappers/SpeciesWrapper.java
@@ -1,6 +1,11 @@
 package etomica.plugin.wrappers;
 
+import java.lang.reflect.Method;
+
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+
 import etomica.species.Species;
+import etomica.species.SpeciesSignature;
 
 public class SpeciesWrapper extends PropertySourceWrapper {
 
@@ -10,5 +15,14 @@ public class SpeciesWrapper extends PropertySourceWrapper {
 
     public PropertySourceWrapper[] getChildren() {
         return new PropertySourceWrapper[]{PropertySourceWrapper.makeWrapper(((Species)object).moleculeFactory())};
+    }
+    
+    protected IPropertyDescriptor makeDescriptor(java.beans.PropertyDescriptor property) {
+
+        if (property.getPropertyType() == SpeciesSignature.class) {
+            return null;
+        }
+        
+        return super.makeDescriptor(property);
     }
 }
