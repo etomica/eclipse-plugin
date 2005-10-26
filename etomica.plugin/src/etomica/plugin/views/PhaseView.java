f18160561bcdb7b5805840b8651cc8b9de520630
Andrew Schultz <ajs42@buffalo.edu>
2005-10-26 01:54:05 +0000

    add PhaseView showing the Atom hierarchy

---
+++ etomica.plugin/src/etomica/plugin/views/PhaseView.java
@@ -0,0 +1,131 @@
+/*
+ * History
+ * Created on Sep 20, 2004 by kofke
+ */
+package etomica.plugin.views;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.part.ViewPart;
+
+import etomica.phase.Phase;
+import etomica.plugin.wrappers.PropertySourceWrapper;
+
+/**
+ * View for listing the species hierarchy via a tree.
+ */
+public class PhaseView extends ViewPart {
+
+	public PhaseView() {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
+	public void createPartControl(Composite parent) {
+		viewer = new TreeViewer(parent);
+//		setViewer(viewer);
+        vcp = new PhaseViewContentProvider();
+		viewer.setContentProvider(vcp);
+		viewer.setLabelProvider(new LabelProvider());
+		hookPageSelection();
+		addKeyListener();
+		createActions();
+		createToolBarButtons();
+//		getViewer().setModel(phase.speciesMaster);
+
+	}
+	
+	private void createToolBarButtons() {
+//		getViewSite().getActionBars().getToolBarManager().add(collapseAction);
+	}
+	
+	//registers as listener so that tree root updates with selections in other views
+	private void hookPageSelection() {
+		pageSelectionListener = new ISelectionListener() {
+			public void selectionChanged(
+					IWorkbenchPart part,
+					ISelection selection) {
+				pageSelectionChanged(part, selection);
+			}
+		};
+		getSite().getPage().addPostSelectionListener(pageSelectionListener);
+	}
+	
+	//causes escape key to deselect all
+	private void addKeyListener() {
+		viewer.getTree().addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent event) {
+				if(event.keyCode == SWT.ESC) {
+					viewer.getTree().deselectAll();
+					viewer.setSelection(null);
+				}
+			}
+		});
+	}
+	
+	/**
+	 * Changes root of tree with change of simulation selected in another view.
+	 */
+	protected void pageSelectionChanged(IWorkbenchPart part, ISelection selection) {
+		if(part == this) return;
+//		System.out.println("SpeciesView selection "+selection.toString());
+		if(!(selection instanceof IStructuredSelection)) return;
+		IStructuredSelection sel = (IStructuredSelection)selection;
+		if(sel.getFirstElement() == null) return;
+		Object obj = ((PropertySourceWrapper)sel.getFirstElement()).getObject();
+        if (obj instanceof Phase) {
+            viewer.setInput(sel.getFirstElement());
+        }
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchPart#setFocus()
+	 */
+	public void setFocus() {
+//		Simulation sim = new Simulation();
+//		phase = new Phase(sim);
+//		Species species = new SpeciesSpheres(sim, 32, 3);
+//		sim.elementCoordinator.go();
+//		viewer.setInput(phase.speciesMaster);
+		viewer.getControl().setFocus();
+		getSite().setSelectionProvider(viewer);
+//		phase = Simulation.instance.phase(0);
+	}
+
+	
+	public TreeViewer getViewer() {
+		return viewer;
+	}
+//	public void setViewer(TreeViewer viewer) {
+//		this.viewer = viewer;
+//	}
+	
+	public void dispose() {
+		if(pageSelectionListener != null) {
+			getSite().getPage().removePostSelectionListener(pageSelectionListener);
+		}
+		super.dispose();
+	}
+	
+	private void createActions() {
+//		collapseAction = new CollapseAllAction(this);
+	}
+		
+	private TreeViewer viewer;
+	private Action collapseAction;
+    private PhaseViewContentProvider vcp;
+	private ISelectionListener pageSelectionListener;
+	private TreeItem root;
+}
---
+++ etomica.plugin/src/etomica/plugin/views/PhaseViewContentProvider.java
@@ -0,0 +1,101 @@
+/*
+ * History
+ * Created on Sep 20, 2004 by kofke
+ */
+package etomica.plugin.views;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+
+import etomica.atom.Atom;
+import etomica.atom.AtomList;
+import etomica.atom.AtomTreeNode;
+import etomica.atom.AtomTreeNodeGroup;
+import etomica.atom.iterator.AtomIteratorListSimple;
+import etomica.phase.Phase;
+import etomica.plugin.wrappers.PropertySourceWrapper;
+
+/**
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class PhaseViewContentProvider implements ITreeContentProvider {
+
+    public PhaseViewContentProvider() {
+        iterator = new AtomIteratorListSimple();
+    }
+	/**
+	 * Simulation is root.
+	 * Controller is child of simulation.
+	 * ActivityGroups are parents of actions/activities
+	 */
+    public Object[] getChildren(Object wrappedElement) {
+        AtomTreeNode node = ((Atom)((PropertySourceWrapper)wrappedElement).getObject()).node;
+        if (!(node instanceof AtomTreeNodeGroup)) {
+            return new Object[0];
+        }
+        return wrapAtomList(((AtomTreeNodeGroup)node).childList);
+    }
+    
+    /**
+     * @param inputElement a linked list containing the simulation instances,
+     * coming from Simulation.getInstances
+     */
+    //the call to viewer.setInput in createPartControl causes the list of
+    //simulation instances to be the input element in this method
+    public Object[] getElements(Object inputElement) {
+        Phase phase = (Phase)((PropertySourceWrapper)inputElement).getObject();
+        AtomList agentList = ((AtomTreeNodeGroup)phase.getSpeciesMaster().node).childList;
+        return wrapAtomList(agentList);
+    }
+    
+    private PropertySourceWrapper[] wrapAtomList(AtomList list) {
+        iterator.setList(list);
+        iterator.reset();
+        PropertySourceWrapper[] wrappers = new PropertySourceWrapper[list.size()];
+        int i=0;
+        while (iterator.hasNext()) {
+            wrappers[i++] = PropertySourceWrapper.makeWrapper(iterator.nextAtom());
+        }
+        return wrappers;
+    }
+
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+	 */
+	public Object getParent(Object element) {
+		System.out.println("SimulationViewContentProvide.getParent");
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+	 */
+	public boolean hasChildren(Object wrappedElement) {
+        return getChildren(wrappedElement).length > 0;
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+	 */
+	public void dispose() {
+        //Simulation.instantiationEventManager.removeListener(this);
+		viewer = null;
+	}
+
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+     */
+    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+        this.viewer = (TreeViewer)viewer;
+        currentSelection = newInput;
+    }
+    
+    Object currentSelection;
+
+	private TreeViewer viewer;
+    private final AtomIteratorListSimple iterator;
+}
