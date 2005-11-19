c336ac24a630997103984a25585caa0ca4308478
Andrew Schultz <ajs42@buffalo.edu>
2005-11-19 02:46:46 +0000

    put the short class name + short description in the combo box.  When a user selects a Species, display the full class name and full description below it.

--- etomica.plugin/src/etomica/plugin/wizards/SpeciesSelector.java
+++ etomica.plugin/src/etomica/plugin/wizards/SpeciesSelector.java
@@ -12,6 +12,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
@@ -20,27 +22,26 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 import etomica.EtomicaInfo;
+import etomica.plugin.ClassDiscovery;
 import etomica.plugin.Registry;
 import etomica.simulation.Simulation;
 import etomica.species.Species;
+
 /**
- * @author Henrique
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Sets up GUI elements to select a new Species
  */
 public class SpeciesSelector extends Composite {
 
-	public Combo species_list = null;
+	public Combo speciesCombo = null;
 	private Label label1 = null;
-	public Text species_name = null;
+	public Text speciesName = null;
 	private Label label3 = null;
 	private HashMap speciesTypeMap = new HashMap();
-	
-	private Label label5 = null;
-	public Species createSpecies(Simulation sim) {
-		int item = species_list.getSelectionIndex();
-		Class speciesClass = (Class) speciesTypeMap.get( species_list.getItem( item ) );
+	private Label speciesDescription = null;
+
+    public Species createSpecies(Simulation sim) {
+		int item = speciesCombo.getSelectionIndex();
+		Class speciesClass = (Class) speciesTypeMap.get( speciesCombo.getItem( item ) );
 		if ( speciesClass==null ) {
             return null;
         }
@@ -65,11 +66,6 @@ public class SpeciesSelector extends Composite {
 		return species;
 	}
 	
-	
-	/**
-	 * @param parent
-	 * @param style
-	 */
 	public SpeciesSelector(Composite parent, int style) {
 		super(parent, style);
 		
@@ -77,16 +73,33 @@ public class SpeciesSelector extends Composite {
 		
 //		 Add all spaces from registry
 		Collection species_from_registry = Registry.queryWhoExtends( etomica.species.Species.class );
-		Iterator item = species_from_registry.iterator(); 
-		while( item.hasNext() )
+		Iterator iterator = species_from_registry.iterator(); 
+		while( iterator.hasNext() )
 		{
-			Class speciesClass = (Class) item.next();
-			EtomicaInfo info = EtomicaInfo.getInfo( speciesClass );
-			species_list.add( info.getShortDescription() );
-			speciesTypeMap.put( info.getShortDescription(), speciesClass );
+			Class speciesClass = (Class)iterator.next();
+			EtomicaInfo info = EtomicaInfo.getInfo(speciesClass);
+            String str = ClassDiscovery.chopClassName(speciesClass.getName())+": "+info.getShortDescription();
+			speciesCombo.add(str);
+			speciesTypeMap.put(str, speciesClass);
 		}
-		int default_selection = species_list.indexOf( "etomica.species.SpeciesSpheresMono");
-		species_list.select( default_selection );
+
+        speciesCombo.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                int item = speciesCombo.getSelectionIndex();
+                if (item == -1) {
+                    return;
+                }
+                Class speciesClass = (Class) speciesTypeMap.get( speciesCombo.getItem( item ) );
+                String str = speciesClass.getName();
+                EtomicaInfo info = EtomicaInfo.getInfo( speciesClass );
+                String longDesc = info.getDescription();
+                if (!str.equals(longDesc)) {
+                    str += ": \n"+longDesc;
+                }
+                speciesDescription.setText(str);
+                SpeciesSelector.this.layout();
+            }
+        });
 
 	}
 
@@ -96,12 +109,12 @@ public class SpeciesSelector extends Composite {
 	 */    
 	private void createCombo() {
 		GridData gridData9 = new org.eclipse.swt.layout.GridData();
-		species_list = new Combo(this, SWT.READ_ONLY | SWT.DROP_DOWN);		   
-		gridData9.horizontalSpan = 2;
+		speciesCombo = new Combo(this, SWT.READ_ONLY | SWT.DROP_DOWN);		   
+		gridData9.horizontalSpan = 1;
 		gridData9.horizontalAlignment = org.eclipse.swt.layout.GridData.FILL;
 		gridData9.verticalAlignment = org.eclipse.swt.layout.GridData.CENTER;
 		gridData9.grabExcessHorizontalSpace = true;
-		species_list.setLayoutData(gridData9);
+		speciesCombo.setLayoutData(gridData9);
 	}
 
     public static void main(String[] args) {
@@ -129,27 +142,30 @@ public class SpeciesSelector extends Composite {
 		GridData gridData6 = new org.eclipse.swt.layout.GridData();
 		GridData gridData5 = new org.eclipse.swt.layout.GridData();
 		GridLayout gridLayout2 = new GridLayout();
-		label1 = new Label(this, SWT.NONE);
-		species_name = new Text(this, SWT.BORDER);
-		label5 = new Label(this, SWT.NONE);
+        gridLayout2.numColumns = 2;
+        gridLayout2.makeColumnsEqualWidth = false;
+        this.setLayout(gridLayout2);
+
+        label1 = new Label(this, SWT.NONE);
+        label1.setText("Species name:");
+        gridData5.horizontalSpan = 1;
+        label1.setLayoutData(gridData5);
+		speciesName = new Text(this, SWT.BORDER);
+        gridData6.horizontalSpan = 1;
+        gridData6.horizontalAlignment = org.eclipse.swt.layout.GridData.FILL;
+        gridData6.verticalAlignment = org.eclipse.swt.layout.GridData.CENTER;
+        gridData6.grabExcessHorizontalSpace = true;
+        speciesName.setLayoutData(gridData6);
+
         label3 = new Label(this, SWT.NONE);
+        label3.setText("Species Type");
+        gridData13.horizontalSpan = 1;
+        label3.setLayoutData(gridData13);
 		createCombo();
-		this.setLayout(gridLayout2);
-		gridLayout2.numColumns = 3;
-		gridLayout2.makeColumnsEqualWidth = false;
-		label1.setText("Species name:");
-		label1.setLayoutData(gridData5);
-		gridData5.horizontalSpan = 1;
-		gridData6.horizontalSpan = 1;
-		gridData6.horizontalAlignment = org.eclipse.swt.layout.GridData.FILL;
-		gridData6.verticalAlignment = org.eclipse.swt.layout.GridData.CENTER;
-		gridData6.grabExcessHorizontalSpace = true;
-		species_name.setLayoutData(gridData6);
-		gridData8.horizontalSpan = 3;
-		label3.setText("Species Type");
-		label3.setLayoutData(gridData13);
-		gridData13.horizontalSpan = 1;
-		label5.setText("");
-		setSize(new org.eclipse.swt.graphics.Point(364,162));
+
+        speciesDescription = new Label(this, SWT.WRAP);
+        gridData8.horizontalSpan = 2;
+        gridData8.horizontalAlignment = org.eclipse.swt.layout.GridData.FILL;
+        speciesDescription.setLayoutData(gridData8);
 	}
-}  //  @jve:decl-index=0:visual-constraint="38,24"
+}
