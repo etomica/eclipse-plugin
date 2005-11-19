2da7a05966c71d49c0fbbe3cb9f138f349d2b061
Andrew Schultz <ajs42@buffalo.edu>
2005-11-19 02:39:09 +0000

    pretend that the ActivityIntegrate and Integrator are a single entity (in the GUI)

--- etomica.plugin/src/etomica/plugin/wrappers/ActivityIntegrateWrapper.java
+++ etomica.plugin/src/etomica/plugin/wrappers/ActivityIntegrateWrapper.java
@@ -1,15 +1,78 @@
 package etomica.plugin.wrappers;
 
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+
 import etomica.action.activity.ActivityIntegrate;
+import etomica.integrator.IntegratorIntervalListener;
+import etomica.integrator.IntegratorNonintervalListener;
+import etomica.simulation.Simulation;
 
 public class ActivityIntegrateWrapper extends PropertySourceWrapper {
 
-    public ActivityIntegrateWrapper(ActivityIntegrate object) {
-        super(object);
+    public ActivityIntegrateWrapper(ActivityIntegrate object, Simulation sim) {
+        super(object,sim);
+        integratorWrapper = (IntegratorWrapper)PropertySourceWrapper.makeWrapper(object.getIntegrator(),sim);
+        setDisplayName(integratorWrapper.toString());
     }
 
-    public PropertySourceWrapper[] getChildren() {
-        return new PropertySourceWrapper[]{
-                PropertySourceWrapper.makeWrapper(((ActivityIntegrate)object).getIntegrator())};
+    public IPropertyDescriptor[] getPropertyDescriptors() {
+        if (descriptors == null) {
+            generateDescriptors();
+            nActivityDescriptors = descriptors.length;
+            IPropertyDescriptor[] integratorDescriptors = integratorWrapper.getPropertyDescriptors();
+            IPropertyDescriptor[] allDescriptors = new IPropertyDescriptor[descriptors.length+integratorDescriptors.length];
+            System.arraycopy(descriptors,0,allDescriptors,0,nActivityDescriptors);
+            System.arraycopy(integratorDescriptors,0,allDescriptors,nActivityDescriptors,allDescriptors.length-nActivityDescriptors);
+            descriptors = allDescriptors;
+        }
+        return descriptors;
     }
+    
+    protected IPropertyDescriptor makeDescriptor(java.beans.PropertyDescriptor property) {
+        String name = property.getDisplayName();  //Localized display name 
+        if(name.equals("integrator")) return null;//skip the integrator since we're pretending to be it
+        return super.makeDescriptor(property);
+    }
+
+
+    public Object getPropertyValue(Object key) {
+        java.beans.PropertyDescriptor pd = (java.beans.PropertyDescriptor)key;
+        for (int i=0; i<descriptors.length; i++) {
+            if (pd == descriptors[i].getId()) {
+                if (i < nActivityDescriptors) {
+                    return super.getPropertyValue(key);
+                }
+                return integratorWrapper.getPropertyValue(key);
+            }
+        }
+        // couldn't find the property descriptor in our own list.  :(
+        return null;
+    }
+
+    public void setPropertyValue(Object key, Object arg1) {
+        java.beans.PropertyDescriptor pd = (java.beans.PropertyDescriptor)key;
+        for (int i=0; i<descriptors.length; i++) {
+            if (pd == descriptors[i].getId()) {
+                if (i < nActivityDescriptors) {
+                    super.setPropertyValue(key,arg1);
+                    return;
+                }
+                integratorWrapper.setPropertyValue(key,arg1);
+                return;
+            }
+        }
+        // couldn't find the property descriptor in our own list.  :(
+    }
+
+    public Class[] getAdders() {
+        return integratorWrapper.getAdders();
+    }
+    
+    public boolean addObjectClass(Simulation sim, Class newObjectClass, Shell shell) {
+        return integratorWrapper.addObjectClass(sim,newObjectClass,shell);
+    }
+    
+    private IntegratorWrapper integratorWrapper;
+    private int nActivityDescriptors;
 }
