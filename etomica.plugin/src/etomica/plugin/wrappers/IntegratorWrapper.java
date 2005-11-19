baa8bd805150b3a1d6f1404b41af57bdcf33db66
Andrew Schultz <ajs42@buffalo.edu>
2005-11-19 02:53:21 +0000

    inoke new IntervalListener wizard

--- etomica.plugin/src/etomica/plugin/wrappers/IntegratorWrapper.java
+++ etomica.plugin/src/etomica/plugin/wrappers/IntegratorWrapper.java
@@ -1,15 +1,28 @@
 package etomica.plugin.wrappers;
 
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+
 import etomica.integrator.Integrator;
 import etomica.integrator.IntegratorIntervalListener;
 import etomica.integrator.IntegratorNonintervalListener;
 import etomica.integrator.IntegratorPhase;
 import etomica.phase.Phase;
+import etomica.plugin.wizards.NewIntervalListenerWizard;
+import etomica.simulation.Simulation;
 
 public class IntegratorWrapper extends PropertySourceWrapper {
 
-    public IntegratorWrapper(Integrator object) {
-        super(object);
+    public IntegratorWrapper(Integrator object, Simulation sim) {
+        super(object,sim);
+    }
+
+    protected IPropertyDescriptor makeDescriptor(java.beans.PropertyDescriptor property) {
+        String name = property.getDisplayName();
+        if(name.equals("potential")) return null;//skip the PotentialMaster
+        
+        return super.makeDescriptor(property);
     }
 
     public boolean removeChild(Object obj) {
@@ -55,6 +68,23 @@ public class IntegratorWrapper extends PropertySourceWrapper {
         }
         else if (obj instanceof Phase && object instanceof IntegratorPhase) {
             return obj == ((IntegratorPhase)object).getPhase();
+        }
+        return false;
+    }
+    
+    public Class[] getAdders() {
+        return new Class[]{IntegratorIntervalListener.class,IntegratorNonintervalListener.class};
+    }
+    
+    public boolean addObjectClass(Simulation sim, Class newObjectClass, Shell shell) {
+        if (newObjectClass == IntegratorIntervalListener.class) {
+            NewIntervalListenerWizard wizard = new NewIntervalListenerWizard((Integrator)object);
+
+            WizardDialog dialog = new WizardDialog(shell, wizard);
+            dialog.create();
+            dialog.getShell().setSize(500,400);
+            dialog.open();
+            return wizard.getSuccess();
         }
         return false;
     }
