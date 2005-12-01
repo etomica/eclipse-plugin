5e0792a7fd27f02305304f25cb9c0a6a497ee97d
Andrew Schultz <ajs42@buffalo.edu>
2005-12-01 19:30:59 +0000

    detabbification just pay attention to start_action and end_action events.  If there is a current action, the controller can be halted.  if not, it can't be halted.

--- etomica.plugin/src/etomica/plugin/actions/StopSimulationActionDelegate.java
+++ etomica.plugin/src/etomica/plugin/actions/StopSimulationActionDelegate.java
@@ -18,59 +18,33 @@ import etomica.plugin.editors.EtomicaEditor;
 public class StopSimulationActionDelegate extends BaseSimulationActionDelegate 
 {
 
-	/**
-	 * 
-	 */
-	public StopSimulationActionDelegate() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IEditorActionDelegate#setActiveEditor(org.eclipse.jface.action.IAction, org.eclipse.ui.IEditorPart)
-	 */
-	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-		current_editor = (EtomicaEditor) targetEditor;
-		current_action = action;
-		
-		// Set the initial value based on the current simulation state
-		if ( current_editor!=null) 
-		{
-			simulation = current_editor.getSimulation();
-			if ( simulation!=null && simulation.getController()!=null )
-			{
-				controller = simulation.getController();
-				action.setEnabled(controller.getCurrentActions().length == 1);
-				controller.addListener( this );
-			}
-		}
-		
-	}
-	
-	/* (non-Javadoc)
-	 * @see etomica.ControllerListener#actionPerformed(etomica.ControllerEvent)
-	 */
-	public void actionPerformed(ControllerEvent event) {
-		if ( event.getType()==ControllerEvent.START )
-		{
-			current_action.setEnabled( true );
-		}
-		else if ( event.getType()==ControllerEvent.NO_MORE_ACTIONS )
-		{
-			current_action.setEnabled( false );
-		}
-		else if ( event.getType()==ControllerEvent.HALTED )
-		{
-			current_action.setEnabled( false );
-		}
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
-	 */
-	public void run(IAction action) {
-		if(controller == null) return;
-		controller.halt();
-	}
+    public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+        current_editor = (EtomicaEditor) targetEditor;
+        current_action = action;
+        
+        // Set the initial value based on the current simulation state
+        if ( current_editor!=null) {
+            simulation = current_editor.getSimulation();
+            if (simulation!=null && simulation.getController()!=null) {
+                controller = simulation.getController();
+                action.setEnabled(controller.getCurrentActions().length == 1);
+                controller.addListener( this );
+            }
+        }
+    }
+    
+    public void actionPerformed(ControllerEvent event) {
+        if (event.getType()==ControllerEvent.START_ACTION) {
+            current_action.setEnabled(true);
+        }
+        else if (event.getType()==ControllerEvent.END_ACTION) {
+            current_action.setEnabled(false);
+        }
+    }
+    
+    public void run(IAction action) {
+        if(controller == null) return;
+        controller.halt();
+    }
 
 }
