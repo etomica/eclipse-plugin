c4872e1b5ce818f14b2c8e87b5849636744d395c
Andrew Schultz <ajs42@buffalo.edu>
2005-12-03 23:35:41 +0000

    try harder to find an appropriate constructor.  Look through all the constructors for one that matches the parameters we're supposed to use.

--- etomica.plugin/src/etomica/plugin/views/ComboClassCellEditor.java
+++ etomica.plugin/src/etomica/plugin/views/ComboClassCellEditor.java
@@ -5,6 +5,11 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.swt.widgets.Composite;
 
+import etomica.data.DataSource;
+import etomica.potential.PotentialMaster;
+import etomica.simulation.Simulation;
+import etomica.space.Space;
+
 /**
  * Special EnumeratedTypeCellEditor that gives the user the option to keep
  * the current object or to select the class of a new object to create. 
@@ -29,21 +34,39 @@ public class ComboClassCellEditor extends ComboCellEditor {
     protected Object doGetValue() {
         if (selection instanceof Class) {
             Class selectedClass = (Class)selection;
-            Class[] parameterClasses = new Class[parameters.length];
-            boolean noConstructor = false;
-            for (int i=0; i<parameters.length; i++) {
-                parameterClasses[i] = parameters[i].getClass();
+            Constructor[] constructors = selectedClass.getConstructors();
+            if (constructors.length == 0) {
+                return null;
             }
+            for (int j=0; j<constructors.length; j++) {
+                Class[] parameterClasses = constructors[j].getParameterTypes();
+                boolean found = true;
+                for (int i=0; i<parameters.length; i++) {
+                    if (!parameterClasses[i].isInstance(parameters[i])) {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found) {
+                    try {
+                        return constructors[j].newInstance(parameters);
+                    } catch (InstantiationException e) {
+                        System.err.println( "Could not instantiate class: " + e.getMessage() );
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        System.err.println( "Illegal access while creating class: " + e.getMessage() );
+                        e.printStackTrace();
+                    }
+                    catch (InvocationTargetException e) {
+                        System.err.println( "Exception creating class: " + e.getMessage() );
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+            }
+            // no constructor found with the given parameters.  Try the default constructor
             try {
-                Constructor constructor = selectedClass.getConstructor(parameterClasses);
-                return constructor.newInstance(parameters);
-            }
-            catch (NoSuchMethodException e) {
-                noConstructor = true;
-            }
-            catch (InvocationTargetException e) {
-                System.out.println("Could not inoke constructor for class "+selectedClass);
-                e.printStackTrace();
+                return selectedClass.newInstance();
             }
             catch (InstantiationException e) {
                 System.out.println("Could not instantiate object of class "+selectedClass);
@@ -52,19 +75,6 @@ public class ComboClassCellEditor extends ComboCellEditor {
             catch (IllegalAccessException e) {
                 System.out.println("Could not instantiate object of class "+selectedClass);
                 e.printStackTrace();
-            }
-            if (noConstructor) {
-                try {
-                    return selectedClass.newInstance();
-                }
-                catch (InstantiationException e) {
-                    System.out.println("Could not instantiate object of class "+selectedClass);
-                    e.printStackTrace();
-                }
-                catch (IllegalAccessException e) {
-                    System.out.println("Could not instantiate object of class "+selectedClass);
-                    e.printStackTrace();
-                }
             }
             return null;
         }
