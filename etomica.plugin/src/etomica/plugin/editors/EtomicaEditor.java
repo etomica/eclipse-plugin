45238c06f10d6d3e316d4241afa0da969aff8a32
Andrew Schultz <ajs42@buffalo.edu>
2006-02-22 20:34:36 +0000

    Away with tabs!

--- etomica.plugin/src/etomica/plugin/editors/EtomicaEditor.java
+++ etomica.plugin/src/etomica/plugin/editors/EtomicaEditor.java
@@ -39,101 +39,99 @@ import etomica.util.EtomicaObjectInputStream;
 
 public class EtomicaEditor extends EditorPart {
 
-	public EtomicaEditor() {
-		super();
-	}
-	public void dispose() {
-		//scene.dispose();
-		
-		if ( this.pageSelectionListener!=null )
-			getSite().getPage().removePostSelectionListener( pageSelectionListener );
-		super.dispose();
-	}
-	
-	/**
+    public EtomicaEditor() {
+        super();
+    }
+    public void dispose() {
+        //scene.dispose();
+        
+        if ( this.pageSelectionListener!=null )
+            getSite().getPage().removePostSelectionListener( pageSelectionListener );
+        super.dispose();
+    }
+    
+    /**
      * Save the contents of this simulation into the file pointed by the IPath object "path". 
-	 * doSaveAs() will ask for a file name and set the "path" variable before calling doSave().
-	 */
-	public void doSave(IProgressMonitor progressMonitor) {
-		// Use XML to stream simulation
-		
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+     * doSaveAs() will ask for a file name and set the "path" variable before calling doSave().
+     */
+    public void doSave(IProgressMonitor progressMonitor) {
+        // Use XML to stream simulation
+        
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
 
-		if (progressMonitor != null)
-			progressMonitor.setCanceled( false );
+        if (progressMonitor != null)
+            progressMonitor.setCanceled( false );
 
         IPath dir = path.removeLastSegments(1);
         IResource resource = workspace.getRoot().findMember(dir);
-		IPath absPath = resource.getLocation();
-		String filename = absPath.append(path.lastSegment()).toOSString();
-		
-		FileOutputStream fos = null;
-		ObjectOutputStream out = null;
-		try
-		{
-			fos = new FileOutputStream( filename);
-		  	out = new ObjectOutputStream(fos);
-		  	out.writeObject( simulation );
-		  	out.close();
+        IPath absPath = resource.getLocation();
+        String filename = absPath.append(path.lastSegment()).toOSString();
+        
+        FileOutputStream fos = null;
+        ObjectOutputStream out = null;
+        try {
+            fos = new FileOutputStream( filename);
+            out = new ObjectOutputStream(fos);
+            out.writeObject( simulation );
+            out.close();
             dirty_flag = false;
             firePropertyChange(PROP_DIRTY);
-		}
-		catch(IOException ex)
-		{
-			ex.printStackTrace();
-		}
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
+        }
 
-	}
-	
+    }
+    
 
-	/**
+    /**
      * Asks for a file name (sets file name in "path" variable) and then makes a runnable thread to call doSave(). 
-	 * @see org.eclipse.ui.ISaveablePart#doSaveAs()
-	 */
-	public void doSaveAs() {
-		Shell shell = getSite().getShell();
+     * @see org.eclipse.ui.ISaveablePart#doSaveAs()
+     */
+    public void doSaveAs() {
+        Shell shell = getSite().getShell();
 
-		SaveAsDialog dialog = new SaveAsDialog(shell);
-		dialog.create();
+        SaveAsDialog dialog = new SaveAsDialog(shell);
+        dialog.create();
 
-		IEditorInput input = getEditorInput();
+        IEditorInput input = getEditorInput();
 
-		IFile original = (input instanceof IFileEditorInput) ? ((IFileEditorInput) input).getFile() : null;
-		if (original != null)
-			dialog.setOriginalFile(original);
+        IFile original = (input instanceof IFileEditorInput) ? ((IFileEditorInput) input).getFile() : null;
+        if (original != null)
+            dialog.setOriginalFile(original);
 
-		if (dialog.open() == Dialog.CANCEL) 
-			return;
+        if (dialog.open() == Dialog.CANCEL) 
+            return;
 
-		IPath filePath = dialog.getResult();
-		if (filePath == null) 
-			return;
+        IPath filePath = dialog.getResult();
+        if (filePath == null) 
+            return;
 
-		// Save the old path - it is subject to non-cancellation - next
-		IPath old_path = path;
-		path = filePath;
-		
-		// Create a progress dialog
-		new ProgressMonitorDialog( shell );
-			
-		// Fire a thread to load our stuff
-		try {
-		    IRunnableWithProgress op = new IRunnableWithProgress() {
-				public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-					doSave( monitor );
-				}
-		    };
-		    // go go go 
-		    new ProgressMonitorDialog( shell ).run(true, true, op);
-		 } catch (InvocationTargetException e) {
-		 	// Handle exception
-		    path=old_path;
-		 } catch (InterruptedException e) {
-		 	// Handle cancellation
-		    path=old_path;
-		 }
-	}
-	
+        // Save the old path - it is subject to non-cancellation - next
+        IPath old_path = path;
+        path = filePath;
+        
+        // Create a progress dialog
+        new ProgressMonitorDialog( shell );
+            
+        // Fire a thread to load our stuff
+        try {
+            IRunnableWithProgress op = new IRunnableWithProgress() {
+                public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+                    doSave( monitor );
+                }
+            };
+            // go go go 
+            new ProgressMonitorDialog( shell ).run(true, true, op);
+         } catch (InvocationTargetException e) {
+             // Handle exception
+            path=old_path;
+         } catch (InterruptedException e) {
+             // Handle cancellation
+            path=old_path;
+         }
+    }
+    
     protected Exception readFromFile( String filename ) {
         FileInputStream fis = null;
         EtomicaObjectInputStream in = null;
@@ -145,22 +143,22 @@ public class EtomicaEditor extends EditorPart {
             in.finalizeRead();
             in.close();
         }
-		catch( Exception ex ) {
+        catch( Exception ex ) {
             WorkbenchPlugin.getDefault().getLog().log(
                     new Status(IStatus.ERROR, PlatformUI.PLUGIN_ID, 0, "Could not read simulation from file " + filename, ex));
-			simulation = null;
+            simulation = null;
             return ex;
-		}
+        }
         if (simulation != null) {
             simulation.clearDataStreams();
             new DataStreamRegister(simulation).registerDataStreams(simulation);
         }
         return null;
-	}
+    }
     
-	public boolean isDirty() {
-		return dirty_flag && !isBusy;
-	}
+    public boolean isDirty() {
+        return dirty_flag && !isBusy;
+    }
     
     /**
      * Marks the editor's simulation as "dirty", meaning it's somewhat 
@@ -184,110 +182,62 @@ public class EtomicaEditor extends EditorPart {
         }
     }
 
-	public boolean isSaveAsAllowed() {
-		return isDirty();
-	}
+    public boolean isSaveAsAllowed() {
+        return isDirty();
+    }
 
-	public void init(IEditorSite site, IEditorInput input)
-			throws PartInitException {
-		this.setSite( site );
-		this.setInput( input );
-		
-		// Create a new simulation
-		//simulation = new Simulation();
+    public void init(IEditorSite site, IEditorInput input)
+            throws PartInitException {
+        this.setSite( site );
+        this.setInput( input );
+        
+        // Create a new simulation
+        //simulation = new Simulation();
 
-		IFile original = (input instanceof IFileEditorInput) ? ((IFileEditorInput) input).getFile() : null;
-		if (original == null)
-			return;
+        IFile original = (input instanceof IFileEditorInput) ? ((IFileEditorInput) input).getFile() : null;
+        if (original == null)
+            return;
 
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IWorkspaceRoot root = workspace.getRoot();
-		path = original.getFullPath();
-		IResource resource = root.findMember(path);
-		if (resource == null) 
-			return;
-		IPath location = resource.getLocation();
-		Exception ex = readFromFile(location.toOSString());
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        IWorkspaceRoot root = workspace.getRoot();
+        path = original.getFullPath();
+        IResource resource = root.findMember(path);
+        if (resource == null) 
+            return;
+        IPath location = resource.getLocation();
+        Exception ex = readFromFile(location.toOSString());
         if (ex != null) {
             throw new PartInitException("Could not read file "+location.toOSString(),ex);
         }
         
-		// Update inner panel 
-		if ( inner_panel != null )
-		{
-			inner_panel.setSimulation( simulation );
-            getSite().setSelectionProvider(inner_panel.getViewer());
-		}
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IWorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
-	 */
-	public void createPartControl(Composite parent) {
-	    if (simulation != null) {
-    		inner_panel = new EtomicaEditorInnerPanel(parent, this, SWT.NONE);
-    		inner_panel.setSimulation( simulation );
+        // Update inner panel 
+        if ( inner_panel != null )
+        {
+            inner_panel.setSimulation( simulation );
             getSite().setSelectionProvider(inner_panel.getViewer());
         }
-	}
-	
-	
-	public Simulation getSimulation()
-	{
-		return simulation;
-	}
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
-	 */
-	public void setFocus() {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	private EtomicaEditorInnerPanel inner_panel;
-	private IPath path = null;
-	private Simulation simulation = null;
-	private ISelectionListener pageSelectionListener;
-	private boolean dirty_flag = false;
-        private boolean isBusy = false;
-	//private java.util.HashMap property_bag_list = new HashMap(8);
-	
-	
-	 public static final void main( String[] args )
-	    {
-	    	String filename = "test.bin";
-			
-			try
-			{
-				FileOutputStream fos = null;
-				ObjectOutputStream out = null;
-				HSMD3D simulation = new HSMD3D();
-				fos = new FileOutputStream( filename);
-			  	out = new ObjectOutputStream(fos);
-			  	out.writeObject( simulation );
-			  	out.close();
-			  	fos.close();
-			  	System.out.println( "Serialization of class HSMD3D succeeded.");
-			}
-			catch(IOException ex)
-			{
-				System.err.println( "Exception:" + ex.getMessage() );
-				ex.printStackTrace();
-			}
-			
-			// Serialize back
-			try
-			{
-				FileInputStream fis = null;
-				ObjectInputStream in = null;
-			  fis = new FileInputStream(filename);
-			  in = new ObjectInputStream(fis);
-			  Simulation simulation = (etomica.simulation.Simulation) in.readObject();
-			  in.close();
-			  fis.close();
-			}
-			catch( Exception ex ) {
-				System.err.println( "Could not read simulation from file " + filename + ". Cause: " + ex.getLocalizedMessage() );
-			}
-	    }
+    public void createPartControl(Composite parent) {
+        if (simulation != null) {
+            inner_panel = new EtomicaEditorInnerPanel(parent, this, SWT.NONE);
+            inner_panel.setSimulation( simulation );
+            getSite().setSelectionProvider(inner_panel.getViewer());
+        }
+    }
+    
+    
+    public Simulation getSimulation() {
+        return simulation;
+    }
+
+    public void setFocus() {
+    }
+
+    private EtomicaEditorInnerPanel inner_panel;
+    private IPath path = null;
+    private Simulation simulation = null;
+    private ISelectionListener pageSelectionListener;
+    private boolean dirty_flag = false;
+    private boolean isBusy = false;
 }
