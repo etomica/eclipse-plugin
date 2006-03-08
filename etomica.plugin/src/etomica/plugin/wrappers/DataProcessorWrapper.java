50d89f64e7f416f64fb91bbaf38ab77c22b58c3d
Andrew Schultz <ajs42@buffalo.edu>
2006-03-08 15:53:53 +0000

    cleanup

--- etomica.plugin/src/etomica/plugin/wrappers/DataProcessorWrapper.java
+++ etomica.plugin/src/etomica/plugin/wrappers/DataProcessorWrapper.java
@@ -1,15 +1,7 @@
 package etomica.plugin.wrappers;
 
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.internal.ExceptionHandler;
-
 import etomica.data.DataProcessor;
 import etomica.data.DataSink;
-import etomica.data.DataSource;
-import etomica.plugin.views.DataSourceView;
-import etomica.plugin.views.DataSourceViewContentProvider;
-import etomica.plugin.views.DataSourceViewLabelProvider;
 import etomica.simulation.Simulation;
 
 public class DataProcessorWrapper extends PropertySourceWrapper {
