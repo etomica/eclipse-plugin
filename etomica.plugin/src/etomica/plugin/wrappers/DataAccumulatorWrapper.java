572a423f138de5820b71fcb8904b085706838cf5
Andrew Schultz <ajs42@buffalo.edu>
2006-03-17 02:12:56 +0000

    add a DataAccumulator so individual accumulators can be reset

---
+++ etomica.plugin/src/etomica/plugin/wrappers/DataAccumulatorWrapper.java
@@ -0,0 +1,33 @@
+package etomica.plugin.wrappers;
+
+import etomica.action.Action;
+import etomica.data.DataAccumulator;
+import etomica.simulation.Simulation;
+
+public class DataAccumulatorWrapper extends DataForkWrapper {
+
+    public DataAccumulatorWrapper(DataAccumulator object, Simulation sim) {
+        super(object, sim);
+    }
+    
+    public Action[] getActions() {
+        return new Action[]{new ResetAction((DataAccumulator)object)};
+    }
+    
+    protected static class ResetAction implements Action {
+        public ResetAction(DataAccumulator accumulator) {
+            this.accumulator = accumulator;
+        }
+        
+        public void actionPerformed() {
+            accumulator.reset();
+        }
+        
+        public String getLabel() {
+            return "Reset";
+        }
+        
+        private final DataAccumulator accumulator;
+    }
+
+}
