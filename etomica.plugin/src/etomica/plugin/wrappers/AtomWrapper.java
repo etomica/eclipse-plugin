93460328c7b183fcf4285fd11319224ba8243a9e
Andrew Schultz <ajs42@buffalo.edu>
2007-02-19 02:16:35 +0000

    don't need to grab the type explicitly.  beans picks it up ok.

--- etomica.plugin/src/etomica/plugin/wrappers/AtomWrapper.java
+++ etomica.plugin/src/etomica/plugin/wrappers/AtomWrapper.java
@@ -17,7 +17,6 @@ public class AtomWrapper extends PropertySourceWrapper implements RemoverWrapper
     
     protected void generateDescriptors() {
         super.generateDescriptors();
-        descriptors = (IPropertyDescriptor[])Arrays.addObject(descriptors,new org.eclipse.ui.views.properties.PropertyDescriptor("type","type"));
         if (((Atom)object).getNode().childAtomCount() > 0) {
             descriptors = (IPropertyDescriptor[])Arrays.addObject(descriptors,new org.eclipse.ui.views.properties.PropertyDescriptor("children","children"));
         }
@@ -35,9 +34,6 @@ public class AtomWrapper extends PropertySourceWrapper implements RemoverWrapper
         }
         String keyString = (String)key;
         PropertySourceWrapper wrapper = null;
-        if (keyString.equals("type")) {
-            return ((Atom)object).getType();
-        }
         if (keyString.equals("children")) {
             Atom[] childAtoms = ((AtomTreeNodeGroup)((Atom)object).getNode()).getChildList().toArray();
             wrapper = PropertySourceWrapper.makeWrapper(childAtoms, simulation, etomicaEditor);
