78dd8da292838467c07ec5ac4dff73712e0e54ce
Andrew Schultz <ajs42@buffalo.edu>
2007-06-16 23:16:22 +0000

    Add a PotentialMasterList wrapper to expose the NeighborListManagers

---
+++ etomica.plugin/src/etomica/plugin/wrappers/PotentialMasterListWrapper.java
@@ -0,0 +1,41 @@
+package etomica.plugin.wrappers;
+
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+
+import etomica.nbr.list.NeighborListManager;
+import etomica.nbr.list.PotentialMasterList;
+import etomica.phase.Phase;
+import etomica.plugin.editors.SimulationObjects;
+
+public class PotentialMasterListWrapper extends PotentialMasterWrapper {
+
+    public PotentialMasterListWrapper(PotentialMasterList object,
+            SimulationObjects simObjects) {
+        super(object, simObjects);
+    }
+
+    protected IPropertyDescriptor[] generateDescriptors() {
+        IPropertyDescriptor[] descriptors = super.generateDescriptors();
+        
+        // add in all of the PotentialMasterList's NeighborListManagers
+        IPropertyDescriptor newDescriptor = makeDescriptor("neighborListManagers", null, NeighborListManager[].class, "NeighborList managers", simObjects);
+        IPropertyDescriptor[] newDescriptors = new IPropertyDescriptor[descriptors.length+1];
+        System.arraycopy(descriptors, 0, newDescriptors, 0, descriptors.length);
+        newDescriptors[descriptors.length] = newDescriptor;
+        
+        return newDescriptors;
+    }
+
+    public Object getPropertyValue(Object key) {
+        if (key instanceof String && ((String)key).equals("neighborListManagers")) {
+            Phase[] phases = simObjects.simulation.getPhases();
+            NeighborListManager[] neighborListManagers = new NeighborListManager[phases.length];
+            for (int i=0; i<phases.length; i++) {
+                neighborListManagers[i] = ((PotentialMasterList)object).getNeighborManager(phases[i]);
+            }
+            return neighborListManagers; //PropertySourceWrapper.wrapArrayElements(neighborListManagers, simObjects, etomicaEditor);
+        }
+        return super.getPropertyValue(key);
+    }
+
+}
